c0b10f925443cbcffbf6dc4626a6ca055694be4f
张以白 <zhangtongding@foxmail.com>
2022-11-02 00:32:27 +0800

    00点32分测试

--- interface_conversion-V0.1.py
+++ interface_conversion-V0.1.py
@@ -5,8 +5,8 @@ from tkinter.filedialog import *
 from tkinter.colorchooser import *
 import random
 import hashlib
-
-"""u8fyhaoiuehfoauaaoiuhf"""
+#00点29分更新
+#更新内容：注释测试
 class Application(Frame):
     def __init__(self, master=None):
         super().__init__(master)
