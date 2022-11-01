f910e0d915e75c188db31dea74b833c9604ef76a
张以白 <zhangtongding@foxmail.com>
2022-11-01 12:53:53 +0800

    添加注释测试

--- interface_conversion-V0.1.py
+++ interface_conversion-V0.1.py
@@ -6,7 +6,7 @@ from tkinter.colorchooser import *
 import random
 import hashlib
 
-"""u8fyhaoiuehfoauehwofuaaoiuhf"""
+"""u8fyhaoiuehfoauaaoiuhf"""
 class Application(Frame):
     def __init__(self, master=None):
         super().__init__(master)
